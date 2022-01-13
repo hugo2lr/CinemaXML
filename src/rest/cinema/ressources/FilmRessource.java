03c44a859ed5c1ed86997a18cf971d03d9a46687
zineb-ch <douniaz.chaouche@hotmail.Fr>
2022-01-13 16:36:52 +0100

    Prog CinemaRessources

--- src/rest/cinema/ressources/FilmRessource.java
+++ src/rest/cinema/ressources/FilmRessource.java
@@ -1,14 +1,21 @@
 package rest.cinema.ressources;
 
+//JAX RS
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.JAXBElement;
 
-import rest.todo.dao.TodoDao;
-import rest.todo.model.Todo;
+import rest.cinema.dao.FilmDao;
+import rest.cinema.model.Film;
 
 public class FilmRessource {
 	@Context
@@ -16,7 +23,7 @@ public class FilmRessource {
     @Context
     Request request;
     String id;
-    public CinemaResource(UriInfo uriInfo, Request request, String id) {
+    public FilmRessource(UriInfo uriInfo, Request request, String id) {
         this.uriInfo = uriInfo;
         this.request = request;
         this.id = id;
@@ -26,9 +33,44 @@ public class FilmRessource {
     @GET
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Film getFilm() {
-        Film film = TodoDao.instance.getModel().get(id);
-        if(todo==null)
-            throw new RuntimeException("Get: Todo with " + id +  " not found");
-        return todo;
+        Film film = FilmDao.instance.getModel().get(id);
+        if(film==null)
+            throw new RuntimeException("Get: Film with " + id +  " not found");
+        return film;
+    }
+    
+ // for the browser
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public Film getFilmHTML() {
+        Film film = FilmDao.instance.getModel().get(id);
+        if(film==null)
+            throw new RuntimeException("Get: Film with " + id +  " not found");
+        return film;
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response putFilm(JAXBElement<Film> film) {
+        Film c = film.getValue();
+        return putAndGetResponse(c);
+    }
+    
+    @DELETE
+    public void deleteFilm() {
+        Film c = FilmDao.instance.getModel().remove(id);
+        if(c==null)
+            throw new RuntimeException("Delete: Film with " + id +  " not found");
+    }
+
+    private Response putAndGetResponse(Film film) {
+        Response res;
+        if(FilmDao.instance.getModel().containsKey(film.getId())) {
+            res = Response.noContent().build();
+        } else {
+            res = Response.created(uriInfo.getAbsolutePath()).build();
+        }
+        FilmDao.instance.getModel().put(film.getId(), film);
+        return res;
     }
 }
